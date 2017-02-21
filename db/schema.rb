fcd3e62888a52dbf55478cfe33a8c5f822fa00dc
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-02-21 14:07:43 +0100

    loading images

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220121834) do
+ActiveRecord::Schema.define(version: 20170221120657) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 25
+    t.string   "title",                                                                 limit: 25
+    t.datetime "created_at",                                                                       null: false
+    t.datetime "updated_at",                                                                       null: false
     t.string   "content"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "#<ActiveRecord::ConnectionAdapters::TableDefinition:0x007ffc8d9b1c98>"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
