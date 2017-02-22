4f83aa061efbaa8ab111d87b093cecae24c174a8
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-02-22 16:29:09 +0100

    configure email

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170221120657) do
     t.datetime "created_at",                                                                       null: false
     t.datetime "updated_at",                                                                       null: false
     t.string   "content"
-    t.string   "#<ActiveRecord::ConnectionAdapters::TableDefinition:0x007ffc8d9b1c98>"
+    t.string   "#<ActiveRecord::ConnectionAdapters::TableDefinition:0x007fcca4d01ec0>"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20170221120657) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
